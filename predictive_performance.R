47f02286d9108bdeb98497047c1c1ad323709f95
L-Groot <lauragroot18@gmail.com>
2024-10-10 16:39:46 +0200

    create PP script

---
+++ predictive_performance.R
@@ -0,0 +1,6 @@
+### Assessing predictive performance ###
+
+# Source Amoroso scripts
+source("https://raw.githubusercontent.com/L-Groot/AmorosoDensity/refs/heads/main/plot_amoroso.R")
+source("https://raw.githubusercontent.com/L-Groot/AmorosoDensity/refs/heads/main/estimate_amoroso.R")
+
